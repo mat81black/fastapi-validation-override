221a37c1c30267c21a2b33eefdc01bde90461f2d
mat81black <mat81black@gmail.com>
2026-07-27 14:13:11 +0200

    ✨ Add public patch_422_responses for schema patching outside override_validation_error

--- fastapi_validation_override/__init__.py
+++ fastapi_validation_override/__init__.py
@@ -1,5 +1,5 @@
-from .overrider import override_validation_error
+from .overrider import override_validation_error, patch_422_responses
 
 __version__ = "0.1.3"
 
-__all__ = ["override_validation_error"]
+__all__ = ["override_validation_error", "patch_422_responses"]
